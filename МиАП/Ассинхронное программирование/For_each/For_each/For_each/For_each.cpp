98b7fc22fde51f651b21d4732f0d85474283cd51
Aleksandra <elaginas27@gmail.com>
2026-01-18 17:53:32 +0300

    Commit 1

---
+++ МиАП/Ассинхронное программирование/For_each/For_each/For_each/For_each.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+#include <algorithm>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+template <typename Iterator, typename Func>
+void parallel_for_each(Iterator begin, Iterator end, Func func, size_t min_block_size = 1000) {
+    size_t size = distance(begin, end);
+
+    if (size < min_block_size) {
+        for_each(begin, end, func);
+        return;
+    }
+
+    Iterator mid = begin;
+    advance(mid, size / 2);
+
+    auto left_future = async(launch::async,
+        parallel_for_each<Iterator, Func>,
+        begin, mid, func, min_block_size);
+
+    parallel_for_each(mid, end, func, min_block_size);
+
+    left_future.wait();
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    vector<int> vec(10000);
+    for (size_t i = 0; i < vec.size(); ++i) {
+        vec[i] = i;
+    }
+
+    auto multiply_by_two = [](int& x) {
+        x *= 2;
+        };
+
+    parallel_for_each(vec.begin(), vec.end(), multiply_by_two);
+
+    cout << "Первые 10 элементов после parallel_for_each: ";
+    for (int i = 0; i < 10; ++i) {
+        cout << vec[i] << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
---
+++ МиАП/Ассинхронное программирование/Сортировка выбором/Сортировка выбором/Сортировка выбором/Сортировка выбором.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+#include <algorithm>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+template <typename Iterator>
+void find_min_async(Iterator begin, Iterator end, promise<Iterator>&& prom) {
+    auto min_it = min_element(begin, end);
+    prom.set_value(min_it); 
+}
+
+template <typename T>
+void selection_sort_async(vector<T>& arr) {
+    for (auto it = arr.begin(); it != arr.end(); ++it) {
+        promise<typename vector<T>::iterator> prom;
+        future<typename vector<T>::iterator> fut = prom.get_future();
+
+        auto search_begin = it;
+        auto search_end = arr.end();
+        async(launch::async, find_min_async<typename vector<T>::iterator>,
+            search_begin, search_end, move(prom));
+
+        auto min_it = fut.get();
+
+        if (min_it != it) {
+            iter_swap(it, min_it);
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    vector<int> arr = { 64, 25, 12, 22, 11, 90, 3, 100, 1 };
+
+    cout << "Изночальный массив: ";
+    for (auto& x : arr) cout << x << " ";
+    cout << "\n";
+
+    selection_sort_async(arr);
+
+    cout << "Отсортированный массив: ";
+    for (auto& x : arr) cout << x << " ";
+    cout << "\n";
+
+    return 0;
+}
