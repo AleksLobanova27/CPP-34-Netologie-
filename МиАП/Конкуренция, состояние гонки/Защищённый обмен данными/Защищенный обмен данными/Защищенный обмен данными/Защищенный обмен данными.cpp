1f9c6aec555960f2a915228fd2fbb5304b767444
Aleksandra <elaginas27@gmail.com>
2026-01-18 16:43:20 +0300

    Commit 2

--- МиАП/Конкуренция, состояние гонки/Защищённый обмен данными/Защищенный обмен данными/Защищенный обмен данными/Защищенный обмен данными.cpp
+++ МиАП/Конкуренция, состояние гонки/Защищённый обмен данными/Защищенный обмен данными/Защищенный обмен данными/Защищенный обмен данными.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-
 class Data {
 private:
     int value;
@@ -30,7 +29,7 @@ public:
     }
 };
 
-// Вариант 1
+// Вариант 1: через lock() с adopt_lock (ваш исходный вариант - правильный)
 void swap1(Data& a, Data& b) {
     lock(a.getMutex(), b.getMutex());
     lock_guard<mutex> lockA(a.getMutex(), adopt_lock);
@@ -38,14 +37,13 @@ void swap1(Data& a, Data& b) {
     a.swapValues(b);
 }
 
-// Вариант 2
+// Вариант 2: через scoped_lock (C++17) - добавляем по заданию
 void swap2(Data& a, Data& b) {
-    lock_guard<mutex> lockFirst(a.getMutex());
-    lock_guard<mutex> lockSecond(b.getMutex());
+    scoped_lock lockBoth(a.getMutex(), b.getMutex());
     a.swapValues(b);
 }
 
-// Вариант 3
+// Вариант 3: через unique_lock с defer_lock (ваш исходный вариант - правильный)
 void swap3(Data& a, Data& b) {
     unique_lock<mutex> lockA(a.getMutex(), defer_lock);
     unique_lock<mutex> lockB(b.getMutex(), defer_lock);
@@ -54,7 +52,6 @@ void swap3(Data& a, Data& b) {
 }
 
 int main() {
-
     setlocale(LC_ALL, "Russian");
     Data data1(10);
     Data data2(20);
@@ -63,23 +60,25 @@ int main() {
     cout << "data1 = " << data1.getValue() << "\n";
     cout << "data2 = " << data2.getValue() << "\n";
 
-    // Тестируем swap1
+    // Тестируем swap1 (lock + lock_guard)
     swap1(data1, data2);
     cout << "\nПосле swap1 (lock + lock_guard):\n";
     cout << "data1 = " << data1.getValue() << "\n";
     cout << "data2 = " << data2.getValue() << "\n";
 
+    // Возвращаем исходные значения
     swap1(data1, data2);
 
-    // Тестируем swap2
+    // Тестируем swap2 (scoped_lock)
     swap2(data1, data2);
-    cout << "\nПосле swap2 (вложенные lock_guard):\n";
+    cout << "\nПосле swap2 (scoped_lock):\n";
     cout << "data1 = " << data1.getValue() << "\n";
     cout << "data2 = " << data2.getValue() << "\n";
 
+    // Возвращаем исходные значения
     swap2(data1, data2);
 
-    // Тестируем swap3
+    // Тестируем swap3 (unique_lock)
     swap3(data1, data2);
     cout << "\nПосле swap3 (unique_lock):\n";
     cout << "data1 = " << data1.getValue() << "\n";
