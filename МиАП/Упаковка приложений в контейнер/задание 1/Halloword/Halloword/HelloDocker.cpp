d9230ef48ed4edc0a995773dd8ab4506400682f5
Aleksandra <elaginas27@gmail.com>
2026-01-21 23:12:15 +0300

    Commit 1

---
+++ МиАП/Упаковка приложений в контейнер/задание 1/Halloword/Halloword/HelloDocker.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello from Docker container!" << std::endl;
+    std::cout << "Compiled with GCC in Ubuntu 16.04" << std::endl;
+    return 0;
+}
---
+++ МиАП/Упаковка приложений в контейнер/задание 2/Calculator/Calculator/Calculator.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+class Calculator {
+public:
+    double add(double a, double b) { return a + b; }
+    double subtract(double a, double b) { return a - b; }
+    double multiply(double a, double b) { return a * b; }
+    double divide(double a, double b) {
+        if (b == 0) throw std::runtime_error("Division by zero!");
+        return a / b;
+    }
+};
+
+int main() {
+    Calculator calc;
+
+    std::cout << "Calculator Test in Docker Container" << std::endl;
+    std::cout << "5 + 3 = " << calc.add(5, 3) << std::endl;
+    std::cout << "5 - 3 = " << calc.subtract(5, 3) << std::endl;
+    std::cout << "5 * 3 = " << calc.multiply(5, 3) << std::endl;
+    std::cout << "6 / 3 = " << calc.divide(6, 3) << std::endl;
+
+    return 0;
+}
