ee82b5ef3aff11f9dca39ddac6655f652634a0ff
Aleksandra <elaginas27@gmail.com>
2026-02-12 17:11:44 +0300

    Commit 1

---
+++ ШП/Структурные шаблоны/Задание 1/Задание 1/Задание 1/Задание 1.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+// Базовый компонент
+class TextComponent {
+public:
+    virtual ~TextComponent() = default;
+    virtual void render(const string& str) = 0;
+};
+
+// Конкретный компонент
+class Text : public TextComponent {
+public:
+    void render(const string& str) override {
+        cout << str;
+    }
+};
+
+// Базовый декоратор
+class TextDecorator : public TextComponent {
+protected:
+    TextComponent* component;
+public:
+    TextDecorator(TextComponent* comp) : component(comp) {}
+    virtual ~TextDecorator() { delete component; }
+};
+
+// Декоратор для параграфа
+class Paragraph : public TextDecorator {
+public:
+    Paragraph(TextComponent* comp) : TextDecorator(comp) {}
+
+    void render(const string& str) override {
+        cout << "<p>";
+        component->render(str);
+        cout << "</p>";
+    }
+};
+
+// Декоратор для перевернутого текста
+class Reversed : public TextDecorator {
+public:
+    Reversed(TextComponent* comp) : TextDecorator(comp) {}
+
+    void render(const string& str) override {
+        string reversed = str;
+        reverse(reversed.begin(), reversed.end());
+        component->render(reversed);
+    }
+};
+
+// Декоратор для ссылки - особый случай, сигнатура render отличается
+class Link : public TextComponent {
+private:
+    TextComponent* component;
+public:
+    Link(TextComponent* comp) : component(comp) {}
+    ~Link() { delete component; }
+
+    void render(const string& href, const string& text) {
+        cout << "<a href=" << href << ">";
+        component->render(text);
+        cout << "</a>";
+    }
+
+    void render(const string& str) override {
+        render("#", str); 
+    }
+};
+
+int main() {
+    // Параграф
+    auto text_block = new Paragraph(new Text());
+    text_block->render("Hello world");
+    cout << endl << endl;
+    delete text_block;
+
+    // Перевернутый текст
+    auto reversed_block = new Reversed(new Text());
+    reversed_block->render("Hello world");
+    cout << endl << endl;
+    delete reversed_block;
+
+    // Ссылка
+    auto link_block = new Link(new Text());
+    link_block->render("netology.ru", "Hello world");
+    cout << endl;
+    delete link_block;
+
+    return 0;
+}
---
+++ ШП/Структурные шаблоны/Задание 2/Задание 2/Задание 2/Задание 2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <memory>
+
+using namespace std;
+
+// Оригинальный класс из лекции
+class VeryHeavyDatabase {
+public:
+    string GetData(const string& key) const noexcept {
+        return "value";
+    }
+};
+
+// Заместитель с ограничением количества обращений
+class OneShotDB {
+private:
+    VeryHeavyDatabase* real_object;
+    size_t shots_left;
+
+public:
+    explicit OneShotDB(VeryHeavyDatabase* real_object, size_t shots = 1)
+        : real_object(real_object), shots_left(shots) {
+    }
+
+    string GetData(const string& key) {
+        if (shots_left > 0) {
+            shots_left--;
+            return real_object->GetData(key);
+        }
+        return "error";
+    }
+};
+
+int main() {
+    auto real_db = VeryHeavyDatabase();
+    auto limit_db = OneShotDB(addressof(real_db), 2);
+
+    cout << limit_db.GetData("key") << endl;
+    cout << limit_db.GetData("key") << endl;
+    cout << limit_db.GetData("key") << endl;
+
+    return 0;
+}
